e4e2d140a0c94c52c3e25c49b71bbf1635c41b51
Elias Lindström <enalindstrom@gmail.com>
2023-02-18 17:34:08 +0100

    VCS setup complete

---
+++ API/BikeAPI/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BikeAPI.Models;
+
+public class Order
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int OrderID { get; set; }
+    public Bike Bike { get; set; }
+    public int ProductID { get; set; }
+}
