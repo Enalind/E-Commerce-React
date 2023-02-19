c3fed8c7345e577b6894f6b9c4a73c6bb8d1d4aa
Elias Lindström <enalindstrom@gmail.com>
2023-02-19 13:00:36 +0100

    finished support for fetching via the API

--- src/pages/productpage.tsx
+++ src/pages/productpage.tsx
@@ -6,27 +6,35 @@ import Product from "../components/product";
 export default function ProductPage() {
     var [isLoading, setIsLoading] = React.useState(true);
     var [products, setProducts] = React.useState([]);
+    var [error, setError] = React.useState(null);
+
 
 
     useEffect(() => {
-        var client = new XMLHttpRequest();
-        client.open("GET", "https://localhost:7282/products");
-        client.onload = function (){
-            setIsLoading(false);
-            setProducts(JSON.parse(client.response));
-        }
-        console.log("Whoooow!");
-        client.send();
-    }, [])
-    return (
-        <>
-        <Navbar />
-        <div className="container">
-            {products.map((product) => (Product(product)))}
-        </div>
-        <div>
-            <h1>Product Page</h1>
-        </div>
-        </>
-    )
+        fetch("https://localhost:7282/products", {method: "GET"})
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();})
+        .then((data) => {setError(null); setProducts(data); })
+        .catch((error) => {console.log(error); setError(error);})
+        .finally(() => {setIsLoading(false);});
+    });
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
+    else{
+        return (
+            <>
+            <Navbar />
+            <div className="container">
+                {products.map((product) => (Product(product)))}
+            </div>
+            <div>
+                <h1>Product Page</h1>
+            </div>
+            </>
+        )
+    }
 }
