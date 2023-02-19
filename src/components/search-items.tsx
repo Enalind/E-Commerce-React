fde35f89fa46eb009d452da2837f942168b1604f
Elias Lindström <enalindstrom@gmail.com>
2023-02-19 16:31:10 +0100

    Added automatic search bar results

--- src/components/search-items.tsx
+++ src/components/search-items.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './search-items.css'
 export default function searchBarItems(searchItems: Array<any>){
     return(
         <div className="search-wrapper">
             {searchItems.map((item, index) => (
-                <div key={index}>{item.name}</div>
+                <div key={index} className="search-item">{item.name} {item.price}$</div>
             ))}
         </div>
     )
