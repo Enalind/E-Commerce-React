9cec9cdd1a411cf0cb7f41abbbe8bb9a08bc7bc5
Elias Lindström <enalindstrom@gmail.com>
2023-02-24 21:08:14 +0100

    Restructured project

---
+++ Admin_Panel/src/components/SideBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './SideBar.css'
+
+export default function SideBar(){
+    return(
+        <div id="sidebar-wrapper">
+            <div className="sidebar-item top-item">
+                <h1 className="sidebar-text">Home</h1>
+                <img/>
+            </div>
+            <div className="sidebar-item">
+                <h1 className="sidebar-text">Home</h1>
+                <img/>
+            </div>
+            <div className="sidebar-item bottom-item">
+                <h1 className="sidebar-text">Home</h1>
+                <img/>
+            </div>
+        </div>
+    )
+}
